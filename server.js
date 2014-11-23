473c448cd19600d235186eadf480973621cca1b8
Sven Mkw <sven.mkw@gmail.com>
2014-11-23 14:41:34 +0100

    routing

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var Session = require("./lib/session");
+var Twitter = require("./lib/twitter");
+
 var app = express();
-var twitterApi = new (require("./lib/twitter"))();
+var twitter = new Twitter();
+var session = new Session();
+var users = twitter.loadTwitterUsers();
+
 
 app.use(express.static("."));
-
-var users = twitterApi.loadTwitterUsers();
 
 app.route('/users/german').get(function(req, res, next) {
   res.json(users.german_users);
@@ -12,6 +16,13 @@ app.route('/users/german').get(function(req, res, next) {
 
 app.route('/users/global').get(function(req, res, next) {
   res.json(users.global_users);
-})
+});
+
+app.route('/next').get(
+  function(req, res, next) {
+    session.getNextQuestion(req, res, next);
+  }, function(req, res, next) {
+  res.json(req.question);
+});
 
 app.listen(3000);
