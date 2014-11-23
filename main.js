e8b4af2f60490f9c693da152792b1e9570517f1b
Nico Ring <ringnico@web.de>
2014-11-23 14:22:55 +0100

    dw

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 function renderCard(tweet) {
   var newCard = document.createElement("tweet-to-guess");
   // newCard.setAttribute("id", "my-element-id");
-
-  newCard.setAttribute("tweet-content", "");
-  document.getElementsByClassName('container')[0].appendChild(newCard);
+  // 
+  newCard.setAttribute("tweet-content", "content");
+  newCard.setAttribute("tweet-title", "title");
+  newCard.setAttribute("tweet-image", "image");
+  document.getElementsByClassName('tweet-container')[0].appendChild(newCard);
 
 }
+
+renderCard();
 
 
 
